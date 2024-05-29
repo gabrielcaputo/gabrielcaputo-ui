4176af9006d46f9188d338c1cf820d56db62eda0
Gabriel Caputo <caputomateus@gmail.com>
2024-05-29 17:14:44 -0300

    Adiciona Storybook e estrutura de pastas e componentes

--- packages/react/src/index.tsx
+++ packages/react/src/index.tsx
@@ -1,5 +1,31 @@
-import { colors } from '@gabrielcaputo-ui/tokens'
+import { ComponentProps } from 'react'
 
-export function App() {
-  return <h1 style={{ color: colors.green300 }}>Hello World</h1>
-}
+import { styled } from './styles'
+
+export const Button = styled('button', {
+  fontFamily: '$default',
+  backgroundColor: '$green300',
+  borderRadius: '$sm',
+  border: 0,
+  fontWeight: 'bold',
+  color: '$white',
+
+  variants: {
+    size: {
+      small: {
+        fontSize: 14,
+        padding: '$2 $4',
+      },
+      big: {
+        fontSize: 16,
+        padding: '$3 $6',
+      },
+    },
+  },
+
+  defaultVariants: {
+    size: 'small',
+  },
+})
+
+export type ButtonProps = ComponentProps<typeof Button>
